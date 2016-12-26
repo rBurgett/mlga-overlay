739d74ad33363cf2e54c1382d487d6716923083e
Ryan <ryan@burgettweb.net>
2016-12-25 20:28:31 -0500

    Replaced arrow functions

--- public/main.js
+++ public/main.js
@@ -1,29 +1,32 @@
-$(document).ready(() => {
+$(document).ready(function() {
 
-    $('#js-input-link').on('click', e => {
+    $('#js-input-link').on('click', function(e) {
         e.preventDefault();
         $('#js-file-input')[0].click();
     });
 
-    $('#js-file-input').on('change', e => {
+    $('#js-file-input').on('change', function(e) {
         e.preventDefault();
         if (e.target.files.length > 0) {
-            const [file] = e.target.files;
+            const file = e.target.files[0];
             const formData = new window.FormData();
             formData.append('imageFile', file);
             const xhr = new window.XMLHttpRequest();
             xhr.open('post', '/', true);
-            xhr.onerror = err => {
+            xhr.onerror = function(err) {
                 console.error(err);
                 swal('Oops!', err.message, 'error');
             };
-            xhr.onload = ({ currentTarget }) => {
+            xhr.onload = function(res) {
+                const currentTarget = res.currentTarget;
                 const imagePath = currentTarget.response;
                 $('#js-input-link').css('display', 'none');
                 $('#js-overlay-image').attr('src', imagePath);
                 $('#js-overlay-image-container').css('display', 'block');
             };
             xhr.send(formData);
+        } else {
+            swal('No file selected', 'You must select a file.', 'warning');
         }
     });
 
